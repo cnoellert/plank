e5ce04348640fe8f468bf04766b970d1113141e6
Alan Latteri <625982+alatteri@users.noreply.github.com>
2026-08-24 14:29:02 -0700

    release: prepare simplified loss indicator

--- scripts/build-client-package-binaries.sh
+++ scripts/build-client-package-binaries.sh
@@ -215,6 +215,7 @@ for required_loss_ui_token in \
   'clampedLoss <= 5.0' \
   '(clampedLoss - 5.0) / 5.0' \
   'QString("%1%").arg(m_PacketLossPercent' \
+  'QRect(174, 16, 52, 17)' \
   'return toolbarLeft() + 229'; do
   rg -Fq "$required_loss_ui_token" \
     "$source_dir/app/streaming/session.cpp" \
